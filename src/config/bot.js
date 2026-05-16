518e680041b6300e6e3203fe38df260e161569d9
Starplayer73844 <rueckerbennet@gmail.com>
2026-05-16 08:58:27 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Team Bambus Bot",
+        name: "",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
